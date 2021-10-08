76628b5d7225e0c9999e610622fd6457d3babec3
Stefan Wilhelm <stefan.wilhelm@macaw.net>
2021-10-08 17:58:28 +0200

    [TASK] Frontend Phasor Integration and init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
-import {Component, OnInit, Renderer2} from '@angular/core';
+import {Component, Input, OnInit, Renderer2} from '@angular/core';
+import Phaser from 'phaser';
+
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'main' });
+  }
+  create() {
+    console.log('create method');
+  }
+  preload() {
+    console.log('preload method');
+  }
+  update() {
+    console.log('update method');
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,25 +23,35 @@ import {Component, OnInit, Renderer2} from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'Tower of Hanoi';
+  phaserGame: Phaser.Game | undefined;
+  config: Phaser.Types.Core.GameConfig | undefined;
 
+  @Input() public start = false;
   public playground: any;
   private lvl: number = 1;
 
   constructor(private renderer: Renderer2) {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      scene: [ MainScene ],
+      parent: 'game',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 100 }
+        }
+      }
+    };
   }
 
   ngOnInit() {
 
   }
 
-  createNewGame() {
-    let tiles = this.lvl + 2;
-
-    for(let x= 0; x<=2; x++){
-      for (let y = 0; y <= tiles-1; y++){
-        this.playground[x][y]='';
-      }
-    }
+  startGame() {
+    this.start = true;
+    this.phaserGame = new Phaser.Game(this.config);
   }
-
 }
